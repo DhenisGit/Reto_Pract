c943e746eac32731859c71ed8e29a042f6320c95
jeanpiere <thelastor02@gmail.com>
2023-07-13 16:53:38 -0500

    cambios

---
+++ app/src/main/java/com/example/retoprcts/LoginnActivity.java
@@ -0,0 +1,14 @@
+package com.example.retoprcts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginn);
+    }
+}
