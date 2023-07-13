ad984f507a0823d2f6724f563cbe4ad258e8f035
jeanpiere <thelastor02@gmail.com>
2023-07-13 18:06:03 -0500

    cambios

--- app/src/main/java/com/example/retoprcts/RegisterActivity.java
+++ app/src/main/java/com/example/retoprcts/RegisterActivity.java
@@ -2,14 +2,26 @@ package com.example.retoprcts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_loginn);
+        Button btnLoginn = findViewById(R.id.btnelogin);
+        btnLoginn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginnActivity .class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
