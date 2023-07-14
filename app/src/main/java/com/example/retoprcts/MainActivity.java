e34483cb11458aa16b0288b2b0e6fa3eb8e5712f
unknowuser33 <brunotorner33@gmail.com>
2023-07-14 10:51:33 -0500

    integracion de mi proyecto

--- app/src/main/java/com/example/retoprcts/MainActivity.java
+++ app/src/main/java/com/example/retoprcts/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.retoprcts;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.retoprcts.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new InicioFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.inicio) {
+                replaceFragment(new InicioFragment());
+            } else if (item.getItemId() == R.id.perfil) {
+                replaceFragment(new PerfilFragment());
+            } else if (item.getItemId() == R.id.cerrar_sesion) {
+                replaceFragment(new CerrarSesionFragment());
+            }
+            return true;
+        });
+    }
+
+    private void  replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
