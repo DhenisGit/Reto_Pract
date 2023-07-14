2947c86d06517e9e83a755a8e6b9499fc86176a2
jeanpiere <thelastor02@gmail.com>
2023-07-14 11:40:20 -0500

    cambios validos

--- app/src/main/java/com/example/retoprcts/LoginnActivity.java
+++ app/src/main/java/com/example/retoprcts/LoginnActivity.java
@@ -1,13 +1,19 @@
 package com.example.retoprcts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.PatternsCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.util.Patterns;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginnActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,27 @@ public class LoginnActivity extends AppCompatActivity {
                 Intent intent = new Intent(LoginnActivity.this, RegisterActivity .class);
                 startActivity(intent);
 
+
             }
+
         });
     }
-}
+
+        private boolean validarEmail() {
+            EditText editTextEmail = findViewById(R.id.email);
+            String email = editTextEmail.getText().toString().trim();
+            boolean emailValido = Patterns.EMAIL_ADDRESS.matcher(email).matches();
+
+            if (emailValido) {
+                Toast.makeText(this, "El correo electrónico es válido", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "El correo electrónico no es válido", Toast.LENGTH_SHORT).show();
+            }
+
+            return emailValido;
+        }
+
+    }
+
+
+
--- app/src/main/java/com/example/retoprcts/RegisterActivity.java
+++ app/src/main/java/com/example/retoprcts/RegisterActivity.java
@@ -1,11 +1,16 @@
 package com.example.retoprcts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.PatternsCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.util.Patterns;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -24,5 +29,18 @@ public class RegisterActivity extends AppCompatActivity {
 
 
 
+    }
+    private boolean validarEmail() {
+        EditText editTextEmail = findViewById(R.id.email2);
+        String email = editTextEmail.getText().toString().trim();
+        boolean emailValido = Patterns.EMAIL_ADDRESS.matcher(email).matches();
+
+        if (emailValido) {
+            Toast.makeText(this, "El correo electrónico es válido", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "El correo electrónico no es válido", Toast.LENGTH_SHORT).show();
+        }
+
+        return emailValido;
     }
 }
